10f3d36813d4012025d648ce59d8cb14ff709548
felipelemos171@gmail.com <felipelemos171@gmail.com@64f47fa1-0f1b-1a00-6e42-6b32c132c8c7>
2010-11-29 19:52:00 +0000

    Criação do controller de perfil

---
+++ trunk/SISGEQ/src/java/br/uern/sisgeq/controller/PerfilController.java
@@ -0,0 +1,68 @@
+package br.uern.sisgeq.controller;
+
+import br.uern.sisgeq.dao.PerfilDao;
+import br.uern.sisgeq.dao.hibernate.PerfilDaoHibernate;
+import br.uern.sisgeq.model.Perfil;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+/**
+ *
+ * @author Felipe Lemos
+ */
+@ManagedBean
+@SessionScoped
+public class PerfilController {
+
+    private Perfil perfil;
+    private DataModel listaPerfis;
+
+    public DataModel getListarPerfis() {
+        List<Perfil> lista = new PerfilDaoHibernate().list();
+        listaPerfis = new ListDataModel(lista);
+        return listaPerfis;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    public void prepararAdicionarPerfil(ActionEvent actionEvent) {
+        perfil = new Perfil();
+    }
+
+    public void prepararAlterarPerfil(ActionEvent actionEvent) {
+        perfil = (Perfil) (listaPerfis.getRowData());
+    }
+
+    public String excluirPerfil() {
+
+        Perfil perfilTemp = (Perfil) (listaPerfis.getRowData());
+        PerfilDao dao = new PerfilDaoHibernate();
+        dao.remove(perfilTemp);
+        return "perfil";
+
+    }
+
+    public void adicionarPerfil(ActionEvent actionEvent) {
+
+        PerfilDao dao = new PerfilDaoHibernate();
+        dao.save(perfil);
+
+    }
+
+    public void alterarPerfil(ActionEvent actionEvent) {
+
+        PerfilDao dao = new PerfilDaoHibernate();
+        dao.update(perfil);
+
+    }
+}
