cd3bdacbe791df1fb4e0cb8cb473df1da4ef8bdb
felipelemos171 <felipelemos171@gmail.com>
2010-11-29 19:52:25 +0000

    Criação do dao de perfil

---
+++ SISGEQ/src/java/br/uern/sisgeq/dao/PerfilDao.java
@@ -0,0 +1,21 @@
+package br.uern.sisgeq.dao;
+
+import br.uern.sisgeq.model.Perfil;
+import java.util.List;
+
+/**
+ *
+ * @author Felipe Lemos
+ */
+public interface PerfilDao {
+
+    public void save(Perfil perfil);
+
+    public Perfil getPerfil(long id);
+
+    public List<Perfil> list();
+
+    public void remove(Perfil perfil);
+
+    public void update(Perfil perfil);
+}
