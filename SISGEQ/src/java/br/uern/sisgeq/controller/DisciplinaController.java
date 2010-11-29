bfc940142ad37ec5a38b897287e48c9f30f74f05
rafaell.gomes <rafaell.gomes@64f47fa1-0f1b-1a00-6e42-6b32c132c8c7>
2010-11-29 03:19:13 +0000

    Remoção do name na classe DisciplinaController e configuração do HibernateUtil

--- SISGEQ/src/java/br/uern/sisgeq/controller/DisciplinaController.java
+++ SISGEQ/src/java/br/uern/sisgeq/controller/DisciplinaController.java
@@ -13,7 +13,7 @@ import javax.faces.model.ListDataModel;
  *
  * @author Felipe Lemos
  */
-@ManagedBean( name="disciplinaManagedBean")
+@ManagedBean
 @SessionScoped
 public class DisciplinaController {
 
