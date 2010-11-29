25d18b78c127d637cfa3c81da666829fc182b5e0
felipelemos171@gmail.com <felipelemos171@gmail.com@64f47fa1-0f1b-1a00-6e42-6b32c132c8c7>
2010-11-29 19:53:27 +0000

    Criação da imp de perfil

---
+++ trunk/SISGEQ/src/java/br/uern/sisgeq/dao/hibernate/PerfilDaoHibernate.java
@@ -0,0 +1,49 @@
+package br.uern.sisgeq.dao.hibernate;
+
+import br.uern.sisgeq.dao.PerfilDao;
+import br.uern.sisgeq.model.Perfil;
+import br.uern.sisgeq.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Felipe Lemos
+ */
+public class PerfilDaoHibernate implements PerfilDao {
+
+    public void save(Perfil perfil) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = session.beginTransaction();
+        session.save(perfil);
+        t.commit();
+    }
+
+    public Perfil getPerfil(long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        return (Perfil) session.load(Perfil.class, id);
+    }
+
+    public List<Perfil> list() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = session.beginTransaction();
+        List lista = session.createQuery("from Perfil").list();
+        t.commit();
+        return lista;
+    }
+
+    public void remove(Perfil perfil) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = session.beginTransaction();
+        session.delete(perfil);
+        t.commit();
+    }
+
+    public void update(Perfil perfil) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = session.beginTransaction();
+        session.update(perfil);
+        t.commit();
+    }
+}
